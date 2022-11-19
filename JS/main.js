672256d2914b38313798a376de341f4be6608139
Parija <parija.jamatia.pj@gmail.com>
2022-11-19 17:06:46 +0530

    queryselector

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,23 @@
 document.title = "DOM Project";
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// console.log(document.forms);
-// console.log(document.links);
-// console.log(document.images);
-
 let headerTitle = document.getElementById('header-title');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Good Morning';
 headerTitle.style.borderBottom = 'solid 2px black';
+//Query selector
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = "green";
 
-//Get element bt class name
-// let items = document.getElementsByClassName('list-group-item');
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = "hidden";
+
+
+//Query selector All
+// let items = document.querySelectorAll('li');
 // for(let i=0;i<items.length;i++){
-//     items[i].style.fontWeight = "bold";
-//     items[i].style.backgroundColor = "green"
+//     items[1].style.backgroundColor = 'green';   
 // }
- 
+let odd = document.querySelectorAll('li:nth-child(odd)');
+let even = document.querySelectorAll('li:nth-child(even)');
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'lightgrey';
+    even[i].style.backgroundColor = 'grey';
 
-//Get element bt tag name
-let items = document.getElementsByTagName('li');
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = "bold";
-    items[i].style.backgroundColor = "yellow"
 }
-
